9af0c89abfb7ff850caee3593e7b41cf8e9bd867
Riiuzaky <jonathans.cruzm@gmail.com>
2025-03-23 16:09:50 -0500

    sin gui pruebas para nube

--- playwright.config.js
+++ playwright.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   timeout: 30000,
   retries: 0,
   use: {
-    headless: false,
+    headless: true,
     viewport: { width: 1280, height: 720 },
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
