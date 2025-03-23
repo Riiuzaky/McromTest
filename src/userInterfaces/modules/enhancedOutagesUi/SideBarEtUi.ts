f0d61729dc60870956fb54c5e2819d3bf0416d0d
Riiuzaky <Riiuzaky@users.noreply.github.com>
2025-03-23 22:46:51 +0000

    deploy: 4199b182ca33e5b810d41a950214b7389e9f98fa

--- src/userInterfaces/modules/enhancedOutagesUi/SideBarEtUi.ts
+++
@@ -1,3 +0,0 @@
-export class SideBarETUi {
-    static btnCloseSideBar = "#cross-circle"; // qyery selector
-}
--- src/userInterfaces/modules/homePageUi/FormProfileUi.ts
+++
@@ -1,6 +0,0 @@
-export class FormProfileUi {
-    static readonly darkModeBtn = 'xpath=//div[@class="toggle-button toggleSelected"]';
-    static readonly LightModeBtn = 'xpath=//div[@class="toggle-button"]';
-    static readonly cancelBtn = 'xpath=//button[@class="btn btn-sm btn-danger cancel_btn"]';
-    static readonly languageBtn = 'xpath=//select[@class="form-control"]';
-  }
